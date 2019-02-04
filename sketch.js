079a40d463d4b723d2c9af450b3f60b91886af67
Matteo Tarda <t_matteo@ymail.com>
2019-02-04 23:11:30 +0100

    Finished assignment (may need testing)

--- sketch.js
+++ sketch.js
@@ -1,11 +1,26 @@
-function preload(){
-  // put preload code here
-}
-
+let p;
+let acc = [0,0]
 function setup() {
   // put setup code here
+    createCanvas(windowWidth,windowHeight);
+    p = [width/2,height/2];
+    ellipseMode(CENTER);
+    noStroke();
+    fill(150)
 }
 
 function draw() {
   // put drawing code here
+    background('#fff5')
+    acc[0] = accelerationX;
+    acc[1] = accelerationY;
+    p[0] += acc[0];
+    p[1] += acc[1];
+    p[0] > width ? p[0] = width : null;
+    p[0] < 0 ? p[0] = 0 : null;
+    p[1] > height ? p[1] = height : null;
+    p[1] < 0 ? p[1] = 0 : null;
+    ellipse(p[0],p[1],width/10)
+    acc[0] > 0 ? acc[0] -= 0.01 : acc[0] += 0.01;
+    acc[1] > 0 ? acc[1] -= 0.01 : acc[1] += 0.01;
 }
